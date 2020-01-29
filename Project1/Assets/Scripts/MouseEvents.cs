26d63989a5d6e620d8e6a7a5ab88c5cbb72c8a27
Hailey Strobelt <haileystrobelt@gmail.com>
2020-01-29 11:12:23 -0700

    MouseEvents

---
+++ Project1/Assets/Scripts/MouseEvents.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Experimental.UIElements;
+
+public class MouseEvents : MonoBehaviour
+{
+    public UnityEvent mouseDownEvent, mouseUpEvent, mouseButtonEvent, mouseDragEvent, mouseOverEvent, mouseExitEvent;
+    
+    private void OnMouseDown()
+    {
+        mouseDownEvent.Invoke();
+    }
+
+    private void OnMouseUp()
+    {
+        mouseUpEvent.Invoke();
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        mouseButtonEvent.Invoke();
+    }
+
+    private void OnMouseDrag()
+    {
+        mouseDragEvent.Invoke();
+    }
+
+    private void OnMouseOver()
+    {
+        mouseOverEvent.Invoke();
+    }
+
+    private void OnMouseExit()
+    {
+        mouseExitEvent.Invoke();
+    }
+}
---
+++ Project1/Assets/Scripts/TriggeringEvents.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggeringEvents : MonoBehaviour
+
+{
+    public int myInt;
+    public float myFloat;
+    public string myString;
+    public UnityEvent myEventOne;
+    public UnityEvent myEventTwo;
+    public UnityEvent myEventThree;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        myEventOne.Invoke();
+        myEventTwo.Invoke();
+        myEventThree.Invoke();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       print((myInt));
+       print((myFloat));
+       print((myString));
+    }
+}
