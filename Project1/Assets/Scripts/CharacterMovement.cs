73e3badfbd6a45bf0555d06a30f096654ee5e20b
Hailey Strobelt <haileystrobelt@gmail.com>
2020-02-03 11:20:44 -0700

    CharacterController

---
+++ Project1/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    private Vector3 positionDirection;
+    public float speed = 5f;
+    public float gravity = -3f;
+    public float jumpForce = 10f;
+    
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        positionDirection.x = Input.GetAxis("Horizontal")*speed;
+        if (Input.GetButtonDown("Jump") && controller.isGrounded)
+        {
+            positionDirection.y = jumpForce;
+           
+        }
+        positionDirection.y += gravity;
+        //positionDirection.z = Input.GetAxis("Horizontal") * speed;
+        controller.Move(positionDirection*Time.deltaTime);
+    }
+}
