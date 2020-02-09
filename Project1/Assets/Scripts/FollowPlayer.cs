69bccb6d7ab949d4d7fc1d31c24ed805b197756e
Hailey Strobelt <haileystrobelt@gmail.com>
2020-02-08 21:30:32 -0700

    Collision Added

---
+++ Project1/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+    
+  
+    
+    void Update()
+    {
+        transform.position = player.position + offset;
+    }
+}
+
+//tutorial from https://www.youtube.com/watch?v=HVB6UVcb3f8 from brackeys
---
+++ Project1/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+
+
+{
+    
+    void OnCollisionEnter (Collision collisionInfo)
+    {
+        Debug.Log(collisionInfo.collider.name);
+    }
+
+   
+}
+
+//tutorial from https://www.youtube.com/watch?v=gAB64vfbrhI
