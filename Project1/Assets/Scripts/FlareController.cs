c8eb9b09c44fe037882fa0a01bdb9be7b41f4598
Hailey Strobelt <haileystrobelt@gmail.com>
2020-02-13 23:41:55 -0700

    Practice Scripting Components

---
+++ Project1/Assets/Scripts/FlareController.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+
+[RequireComponent(typeof(Flare))]
+
+public class FlareController : MonoBehaviour
+{
+    private Flare flareObj;
+
+    void Start()
+    {
+        flareObj = GetComponent<Flare>();
+        flareObj.GetInstanceID();
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Project1/Assets/Scripts/GUIController.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+
+[RequireComponent(typeof(GUI))]
+
+public class GUIController : MonoBehaviour
+{
+    private GUI guiObj;
+    
+    
+    void Start()
+    {
+        guiObj = GetComponent<GUI>();
+        var ns = guiObj.GetType().Namespace;
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
