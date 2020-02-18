4647909103f8c841598f3016c3578fc24585a4c3
Hailey Strobelt <haileystrobelt@gmail.com>
2020-02-17 18:55:33 -0700

    CharacterModifier Tutorial

---
+++ Project1/Assets/Scripts/CharacterStat.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStat : MonoBehaviour
+{
+    public float BaseValue;
+
+    private readonly List<StatModifier> statModifiers;
+
+    public CharacterStat(float baseValue)
+    {
+        BaseValue = baseValue;
+        statModifiers = new List<StatModifier>();
+    }
+
+
+}
+
+//tutorial from https://www.youtube.com/watch?time_continue=3&v=SH25f3cXBVc&feature=emb_title
---
+++ Project1/Assets/Scripts/StatModifier.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+
+public class StatModifier : MonoBehaviour
+{
+    public readonly float Value;
+
+    public StatModifier(float value)
+    {
+        Value = value;
+    }
+}
+
+//tutorial from https://www.youtube.com/watch?time_continue=3&v=SH25f3cXBVc&feature=emb_title
