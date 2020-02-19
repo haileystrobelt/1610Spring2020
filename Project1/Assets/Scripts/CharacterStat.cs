8fb8363c92d219c24a0b28428424645569c4f7ed
Hailey Strobelt <haileystrobelt@gmail.com>
2020-02-18 21:46:22 -0700

    CharacterModifier Edits

--- Project1/Assets/Scripts/CharacterStat.cs
+++ Project1/Assets/Scripts/CharacterStat.cs
@@ -1,11 +1,46 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterStat : MonoBehaviour
 {
     public float BaseValue;
+    
+    public float Value
+    {
+        get { return CalculateFinalValue(); }
+    }
 
+    private bool isDirty = true;
+    private float _value;
+
+    private readonly List<StatModifier> _statModifiers;
+    
     private readonly List<StatModifier> statModifiers;
+    
+    
+    private float CalculateFinalValue()
+    {
+        float finalValue = BaseValue;
+
+        for (int i = 0; i < statModifiers.Count; i++)
+        {
+            finalValue += statModifiers[1].Value;
+        }
+
+        return (float) Math.Round(finalValue, 4);
+    }
+
+    public void AddModifier(StatModifier mod)
+    {
+        statModifiers.Add(mod);
+    }
+    
+    public bool RemoveModifier(StatModifier mod)
+    {
+        statModifiers.Remove(mod);
+    }
+    
 
     public CharacterStat(float baseValue)
     {
--- Project1/Assets/Scripts/StatModifier.cs
+++ Project1/Assets/Scripts/StatModifier.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 public class StatModifier : MonoBehaviour
 {
     public readonly float Value;
+    public readonly int Order;
 
     public StatModifier(float value)
     {
         Value = value;
     }
+
 }
 
 //tutorial from https://www.youtube.com/watch?time_continue=3&v=SH25f3cXBVc&feature=emb_title
