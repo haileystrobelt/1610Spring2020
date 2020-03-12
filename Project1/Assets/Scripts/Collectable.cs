9329a80af871161cf7991714a28cbd6b63a28659
Hailey Strobelt <haileystrobelt@gmail.com>
2020-03-11 19:38:12 -0600

    Arrays and Collections

---
+++ Project1/Assets/Scripts/Collectable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+
+public class Collectable : ScriptableObject
+{
+    public int powerLevel = 10;
+    public Color collectableColor = Color.red;
+    public Sprite collectableSprite;
+}
---
+++ Project1/Assets/Scripts/ListAndLoops.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class ListAndLoops : ScriptableObject
+{
+    
+    public List<string> stringList;
+    public string singleString;
+
+    public void AddtoList(string stringObj)
+    {
+        stringList.Add(stringObj);
+    }
+
+
+    public void RemoveFromList(string stringObj)
+    {
+        for (int i = 0; i < stringList.Count; i++)
+        {
+            if (stringList[i] == stringObj)
+            {
+                stringList.Remove(stringObj);
+            }
+        }
+    }
+
+
+    public void SortList()
+    {
+        stringList.Sort();
+    }
+    
+    
+    public void CheckList ()
+    {
+        foreach (var obj in stringList)
+        {
+            if (obj.Contains(singleString))
+            {
+                Debug.Log(obj);
+            }
+        }
+    }
+
+}
