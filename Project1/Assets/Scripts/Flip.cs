cc6e255b0064152be28e6704ccff4483b55cb936
Hailey Strobelt <haileystrobelt@gmail.com>
2020-04-08 12:41:39 -0600

    Character Flip

---
+++ Project1/Assets/Scripts/Flip.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flip : MonoBehaviour
+{
+    public KeyCode key1 =KeyCode.LeftArrow, key2 = KeyCode.RightArrow;
+    public float direction1 = 0, direction2 = 180;
+   
+ 
+    void Update()
+    {
+        if (Input.GetKeyDown((KeyCode.LeftArrow))
+        {
+            transform.rotation = Quaternion.identity;
+        }
+
+        if (Input.GetKeyDown((KeyCode.RightArrow)
+        {
+            transform.rotation = Quaternion.Euler(x: 0, y: 180, z:0);
+        }
+    }
+
+    public void FlipRotate(float value)
+    {
+        transform.Rotate(xAngle:0, yAngle:value, zAngle:0);   
+    }
+}
