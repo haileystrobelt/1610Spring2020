c2bbcdebe7bfa31db3003b70097275147088c3d7
Hailey Strobelt <haileystrobelt@gmail.com>
2020-04-20 23:24:22 -0600

    Audio

---
+++ Project1/Assets/Scripts/Health.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Health : MonoBehaviour
+{
+    public int healthValue = 10;
+    public AudioSource sound;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        healthValue--;
+        if (healthValue > 0)
+            return;
+        
+        sound.Play();
+        Destroy(gameObject, 0.5f);
+    }
+
+}
